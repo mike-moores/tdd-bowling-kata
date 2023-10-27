5b7773509808d6d4ff1d6068b36b897a00c38297
mike moores <michaelmoores@hotmail.com>
2023-10-28 11:34:28 +1300

    completed testing

--- score.js
+++ score.js
@@ -2,6 +2,23 @@ export {
   scoreFrame
 }
 
-function scoreFrame(frame) {
-  return frame[0] + frame[1]
+function scoreFrame(frame, nextFrame, nextNextFrame) {
+  let score = frame.reduce((frameScore, ball) => frameScore + ball, 0)
+  if (isSpare(frame)) {
+    score += nextFrame[0]
+  } else if (isStrike(frame)) {
+    score += nextFrame.reduce((frameScore, ball) => frameScore + ball, 0)
+    if (isStrike(frame) && isStrike(nextFrame)) {
+      score += nextNextFrame[0]
+    }
+  }
+  return score
+}
+
+function isStrike(frame) {
+  return frame[0] === 10
+}
+
+function isSpare(frame) {
+  return frame[0] + frame[1] === 10 && frame[0] !== 10
 }
--- score.test.js
+++ score.test.js
@@ -9,7 +9,6 @@ test('scores a gutterball frame', () => {
   const frame = [0, 0]
   const expected = 0
   const actual = score.scoreFrame(frame)
-
   expect(actual).toBe(expected)
 })
 
@@ -17,6 +16,34 @@ test('scores a normal frame', () => {
   const frame = [2, 3]
   const expected = 5
   const actual = score.scoreFrame(frame)
+
+  expect(actual).toBe(expected)
+})
+
+test('scores a spare frame', () => {
+  const frame = [1, 9]
+  const nextBall = [2]
+  const expected = 12
+  const actual = score.scoreFrame(frame, nextBall)
+
+  expect(actual).toBe(expected)
+})
+
+test('scores a strike', () => {
+  const frame = [10, 0]
+  const nextFrame = [3, 6]
+  const expected = 19
+  const actual = score.scoreFrame(frame, nextFrame)
+
+  expect(actual).toBe(expected)
+})
+
+test('scores a double strike', () => {
+  const frame = [10, 0]
+  const nextFrame = [10, 0]
+  const nextNextFrame = [3, 5]
+  const expected = 23
+  const actual = score.scoreFrame(frame, nextFrame, nextNextFrame)
 
   expect(actual).toBe(expected)
 })
